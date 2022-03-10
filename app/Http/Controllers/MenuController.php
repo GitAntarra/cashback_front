0ce2bcf5fd2de5d91f30f3e1b2821ae99867e594
anttaracoding <andra.manday@gmail.com>
2022-03-10 16:55:38 +0700

    mantap

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 use Illuminate\Http\Request;
 namespace App\Http\Controllers;
 use App\HttpRequest;
@@ -13,6 +12,60 @@ class MenuController extends Controller
     public function __construct()
     {
         $this->HttpRequest = new HttpRequest;  
+    }
+
+    public function selectionmenu(Request $request) {
+        $res = $this->HttpRequest("GET", "/menus/selected");
+        $getPost = $request->post();
+        $uker = $getPost['uker'] ? $getPost['uker'] : 'Kantor Pusat';
+        $level = $getPost['level'] ? $getPost['level'] : 'SUPERADMIN';
+
+        if( isset( $getPost['id'] ) ){
+            $param = array(
+                "id" => $getPost['id'],
+                "uker" => $getPost['uker'],
+                "level" => $getPost['level'],
+            );
+            $resChecked = $this->HttpRequest("POST", "/menus/check-uncheck", $param);
+            if(empty($resChecked->ok())){
+                Session::flash('success','action success');
+            }else{
+                return $res;
+            }
+        }
+
+        if(!empty($res->json())){
+            $i=0;
+            $mtp = array();
+            foreach($res->json() as $row){
+                $checked = 0;
+                $idSelected = null;
+                foreach($row['menuSelected'] as $b){
+                if($b['uker']== $uker && $b['level']==$level){
+                    $checked = 1;
+                    $idSelected = $b['id'];
+                }
+                }
+                $mtp[$i++] = array(
+                "id" => $row['id'],
+                "type" => $row['type'],
+                "name" => $row['name'],
+                "url" => $row['url'],
+                "i18n" => $row['i18n'],
+                "icon" => $row['icon'],
+                "checked" => $checked,
+                "idselected" => $idSelected
+                );
+            }
+            $data['lists'] = $mtp;
+            $data['pageConfigs'] = ['pageHeader' => true];
+            $data['breadcrumbs'] = [
+                ["link" => "/", "name" => "Home"],["name" => "Selecting Menu"]
+            ];
+            return view('menus.menu-select-list',$data);
+        }
+
+        return $res;
     }
 
     //Menu List
@@ -69,67 +122,3 @@ class MenuController extends Controller
         return view('menu.menu-selected');
     }
 }
-=======
-namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-use Redirect;
-use Session;
-
-class MenuController extends Controller
-{
-
-    public function selectionmenu(Request $request) {
-        $res = $this->HttpRequest("GET", "/menus/selected");
-        $getPost = $request->post();
-        $uker = $getPost['uker'] ? $getPost['uker'] : 'Kantor Pusat';
-        $level = $getPost['level'] ? $getPost['level'] : 'SUPERADMIN';
-
-        if( isset( $getPost['id'] ) ){
-            $param = array(
-                "id" => $getPost['id'],
-                "uker" => $getPost['uker'],
-                "level" => $getPost['level'],
-            );
-            $resChecked = $this->HttpRequest("POST", "/menus/check-uncheck", $param);
-            if(empty($resChecked->ok())){
-                Session::flash('success','action success');
-            }else{
-                return $res;
-            }
-        }
-
-        if(!empty($res->json())){
-            $i=0;
-            $mtp = array();
-            foreach($res->json() as $row){
-                $checked = 0;
-                $idSelected = null;
-                foreach($row['menuSelected'] as $b){
-                if($b['uker']== $uker && $b['level']==$level){
-                    $checked = 1;
-                    $idSelected = $b['id'];
-                }
-                }
-                $mtp[$i++] = array(
-                "id" => $row['id'],
-                "type" => $row['type'],
-                "name" => $row['name'],
-                "url" => $row['url'],
-                "i18n" => $row['i18n'],
-                "icon" => $row['icon'],
-                "checked" => $checked,
-                "idselected" => $idSelected
-                );
-            }
-            $data['lists'] = $mtp;
-            $data['pageConfigs'] = ['pageHeader' => true];
-            $data['breadcrumbs'] = [
-                ["link" => "/", "name" => "Home"],["name" => "Selecting Menu"]
-            ];
-            return view('menus.menu-select-list',$data);
-        }
-
-        return $res;
-    }
-}
->>>>>>> 812035441c94c738ebdc55b4ed3396d5081aff07
