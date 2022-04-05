d7edcf1c23b0026f038951aa97840b9e4bd528f6
partarutungdoau <vickimanurung16@gmail.com>
2022-04-05 08:32:31 +0700

    pushlocal5april

--- resources/views/voucher/voucher-list.blade.php
+++ resources/views/voucher/voucher-list.blade.php
@@ -24,7 +24,8 @@
             <input type="text" Placeholder="Search" class="form-control">   
             <div class="input-group-append">
               <button type="submit" class="btn btn-primary" title="Find"><i class="bx bx-search text-white"> Find</i></button>
-              <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addModalform" title="Add Feature"><i class="bx bx-plus text-white">Add Voucher</i></button>
+              <button type="button" class="btn btn-info" data-toggle="modal"
+              data-target="#addVoucherModal" title="Add Feature"><i class="bx bx-plus text-white">Add Voucher</i></button>
             </div>
           </div>
         </div>
@@ -37,42 +38,22 @@
         @if(!empty($voucherData) && isset($voucherData))
         @foreach($voucherData as $row)
         <div class="col-xl-4 col-sm-6 col-12">
-          <a href="">
-            <div class="card border-info bg-transparent">
-              <div class="card-content">
-                <div class="row">
-                  <div class="col-lg-4 ">
-                    <div class="row">
-                      <div class="col-12 text-center justify-content-center">
-                        <h2 class="">30%</h2>
-                      </div>
-                      <div class="col-12 text-center">
-                        <span class="badge badge-light-primary">CODE123</span>
-                      </div>
-                      <div class="col-12 text-center p-1">
-                        <label for="">BRIMO</label>
-                      </div>
-                    </div>
-                  
-                  </div>
-                  <div class="col-lg-8 col-md-12">
-                    <div class="row pt-1 ">
-                      <div class="col-md-5 p-0 m-0 profile-widget-name"> Limit  </div>
-                      <div class="col-md-7 text-left m-0 p-0">: 3 </div>
-                      <div class="col-md-5 p-0 m-0 profile-widget-name"> Min Transaction  </div>
-                      <div class="col-md-7 text-left m-0 p-0">: 50.000 </div>
-                      <div class="col-md-5 p-0 m-0 profile-widget-name"> Max Redeem </div>
-                      <div class="col-md-7 text-left m-0 p-0">: 3 </div>
-                      <div class="col-md-5 p-0 m-0 profile-widget-name"> Code  </div>
-                      <div class="col-md-7 text-left m-0 p-0">: CODE123 </div>
-                      <div class="col-md-5 p-0 m-0 profile-widget-name"> Due Date  </div>
-                      <div class="col-md-7 text-left m-0 p-0">: 21 April 2022 </div>
-                    </div>
+          <div class="card border-info">
+            <div class="card-content">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-3">
+                  <img src="{{asset('images/banner/banner-35.jpg')}}" class="h-100 w-100 rounded-left">
+                </div>
+                <div class="col-md-12 col-lg-9">
+                  <div class="card-body pt-1 pl-1">
+                    <span><b>{{$row['type']}} {{$row['percent']}} %</b></span>
+                    <p>s/d : {{date('d-m-Y', strtotime($row['dueDate']))}}</p>
+                    Code : <span class="badge badge-light-info">{{$row['code']}}</span>
                   </div>
                 </div>
               </div>
             </div>
-          </a>
+          </div>
         </div>
         @endforeach
         @else
@@ -101,91 +82,153 @@
   </div>
 </section>
 
-<!--Edit Feature Modal -->
-<div class="modal fade text-left" id="editModalForm" tabindex="-1" role="dialog"
+<!--scrolling content Modal -->
+<div class="modal fade text-left" id="addVoucherModal" tabindex="-1" role="dialog"
   aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable" role="document">
+  <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-      <h4 class="modal-title" id="exampleModalScrollableTitle">Add Feature </h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <i class="bx bx-x"></i>
-          </button>
+        <h5 class="modal-title" id="exampleModalScrollableTitle">Create Voucher</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <i class="bx bx-x"></i>
+        </button>
       </div>
       <div class="modal-body">
-      <form action="{{ route('edit-feature.post') }}" method="post">
-          @csrf
-        <input type="text" name="editFeature" id="editFeature" value="editFeature" hidden> 
-        <input type="text" name="idFeature" id="idFeature" value="idFeature" class="form-control" hidden>
-        <div class="col-12" id="formname">
-          <label>Nama Feature </label>
-          <div class="form-group">
-            <input name="featureName" id="featureName" type="text" placeholder="Feature Name" class="form-control" require>
+      <form method="post" action="{{route('createvoucher.post')}}" id="form_input">
+        @csrf
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>CODE</label>
+                </div>
+                <div class="col-9">
+                <input require class="form-control" name="vouchercode" id="vouchercode" placeholder="Voucher Code" type="text" onkeyup="
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+                this.value = this.value.toUpperCase();
+                this.setSelectionRange(start, end);
+                ">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>TYPE</label>
+                </div>
+                <div class="col-9">
+                <input require type="text" name="type" id="type" value="CASHBACK" class="form-control" readonly>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>CHANNEL</label>
+                </div>
+                <div class="col-9">
+                <select name="channel" id="channel" class="custom-select">
+                  @foreach ($list_channel as $row)
+                    <option value="{{$row['channel_id']}}">{{$row['channel_id']}}</option>
+                  @endforeach
+                </select>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>FEATURE</label>
+                </div>
+                <div class="col-9">
+                <select name="channel" id="channel" class="custom-select">
+                  @foreach ($list_feature as $row)
+                    <option value="{{$row['id']}}">{{$row['feature_id']}}</option>
+                  @endforeach
+                </select>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>QUOTA</label>
+                </div>
+                <div class="col-9">
+                    <input require type="number" name="limit" id="limit" class="touchspin">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+          <div class="row">
+            <div class="col-3">
+                <label for="start_date">START DATE</label>
+            </div>
+            <div class="col-9">
+              <input type="datetime-local" name="startdate" id="startdate" class="form-control" min="<?= date("Y-m-dTH:i:s");?>" max="2022-11-16T21:25:33"/>
+            </div>
           </div>
         </div>
-        <div class="col-12" id="formurl">
-            <div class="form-group">
-                <label>Description</label>
-                <fieldset class="form-group">
-                    <textarea class="form-control" id="description" name="description" rows="3" placeholder="Description"></textarea>
-                </fieldset>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>DUE DATE</label>
+                </div>
+                <div class="col-9">
+                <input require type="datetime-local" name="duedate" id="duedate" class="form-control" min="1">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>MINIMAL TRANSACTION</label>
+                </div>
+                <div class="col-9">
+                    <input require type="text" name="mintransaction" id="mintransaction" class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>MAXIMAL CASHBACK</label>
+                </div>
+                <div class="col-9">
+                    <input require type="text" name="maxpotency" id="maxpotency" class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>PERCENT</label>
+                </div>
+                <div class="col-9">
+                <input require type="text" name="percent" id="percent" class="touchspin"  data-bts-postfix="%" />
+                </div>
+            </div>
+        </div>
+        <div class="col-12 pb-1">
+            <div class="row">
+                <div class="col-3">
+                    <label>MAXIMAL REDEEM</label>
+                </div>
+                <div class="col-9">
+                    <input require type="number" name="maxredeem" id="maxredeem" class="touchspin" min="1" value="1">
+                </div>
             </div>
         </div>
       </div>
       <div class="modal-footer">
-            <button type="button" class="btn btn-light-secondary" data-dismiss="modal">
-              <i class="bx bx-x d-block d-sm-none"></i>
-              <span class="d-none d-sm-block">Close</span>
-            </button>
-            <button type="submit" class="btn btn-primary ml-1">
-              <i class="bx bx-check d-block d-sm-none"></i>
-              <span class="d-none d-sm-block">Add</span>
-            </button>
-      </form>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!--Add Feature Modal -->
-<div class="modal fade text-left" id="addModalform" tabindex="-1" role="dialog"
-  aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-      <h4 class="modal-title" id="exampleModalScrollableTitle">Add Feature </h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <i class="bx bx-x"></i>
-          </button>
-      </div>
-      <div class="modal-body">
-      <form action="{{ route('addFeature.post') }}" method="post">
-          @csrf
-        <input type="text" name="addFeature" id="addFeature" value="addFeature" hidden> 
-        <div class="col-12" id="formname">
-          <label>Nama Feature </label>
-          <div class="form-group">
-            <input name="featureName" id="featureName" type="text" placeholder="Feature Name" class="form-control" require>
-          </div>
-        </div>
-        <div class="col-12" id="formurl">
-            <div class="form-group">
-                <label>Description</label>
-                <fieldset class="form-group">
-                    <textarea class="form-control" id="description" name="description" rows="3" placeholder="Description"></textarea>
-                </fieldset>
-            </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-            <button type="button" class="btn btn-light-secondary" data-dismiss="modal">
-              <i class="bx bx-x d-block d-sm-none"></i>
-              <span class="d-none d-sm-block">Close</span>
-            </button>
-            <button type="submit" class="btn btn-primary ml-1">
-              <i class="bx bx-check d-block d-sm-none"></i>
-              <span class="d-none d-sm-block">Add</span>
-            </button>
+        <button type="button" class="btn btn-light-secondary" data-dismiss="modal">
+          <i class="bx bx-x d-block d-sm-none"></i>
+          <span class="d-none d-sm-block">Close</span>
+        </button>
+        <button type="submit" class="btn btn-primary ml-1">
+          <i class="bx bx-check d-block d-sm-none"></i>
+          <span class="d-none d-sm-block">Create</span>
+        </button>
       </form>
       </div>
     </div>
@@ -198,92 +241,97 @@
 <script type="text/javascript">
   $.ajaxSetup({
     headers: {
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+  var mintrans = document.getElementById("mintransaction");
+  
+  mintrans.addEventListener("keyup", function(e) {
+    mintrans.value = formatMinTrans(this.value, "Rp. ");
   });
 
-  $(document).ready(function () {
-    $('.EditFeatureButton').on('click', function (){
-      
-      let as = document.querySelector('.EditMenuButton');
-      let idFeature = $(this).attr("idFeature");
-      
-      $.ajax({
-        type: "GET",
-        url : "{{asset('/view-feature')}}?id="+idFeature,
-        success : function(data){
-          $('#editModalForm').modal('show');
-          $('#idFeature').val(data.id)
-          $('#featureName').val(data.feature_id);
-          $('#description').val(data.description);
-        }
-      });
-      return false;
-    });
-    
-    $('.prev').on('click', function (){
-      alert("123");
-    });
+  /* Format Tanggal*/
+  function myFunction() {
+    var x = document.getElementById("startdate").min = "2006-05-05T16:15:23";
+  }
 
-  $('.confirmdel').on('click', function () {
-        let idFeature = $(this).attr("idFeature");
-        Swal.fire({
-          title: 'Are you sure?',
-          text: "You want to delete this Feature?",
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Yes, delete it!',
-          confirmButtonClass: 'btn btn-warning',
-          cancelButtonClass: 'btn btn-danger ml-1',
-          buttonsStyling: false,
-        }).then(function (result) {
-          if (result.value) {
-            $.ajax({
-              type  : "GET",
-              url   : "{{asset('/delete-feature')}}",
-              data  : {
-                  idFeature : idFeature,
-              },
-              success: function(response) {
-                Swal.fire({
-                  type: "success",
-                  title: 'Deleted!',
-                  text: 'Your file has been deleted.',
-                  confirmButtonClass: 'btn btn-success',
-                }).then((w) =>{
-                    location.reload(true);
-                });
-              },
-              failure: function (response) {
-                  swal(
-                  "Internal Error",
-                  "Oops, your note was not saved.", // had a missing comma
-                  "error"
-                  )
-              }
-            });
+  /* Fungsi formatMinTrans */
+  function formatMinTrans(angka, prefix) {
+    var number_string = angka.replace(/[^,\d]/g, "").toString(),
+      split = number_string.split(","),
+      sisa = split[0].length % 3,
+      mintrans = split[0].substr(0, sisa),
+      ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+    if (ribuan) {
+      separator = sisa ? "." : "";
+      mintrans += separator + ribuan.join(".");
+    }
+
+    mintrans = split[1] != undefined ? mintrans + "," + split[1] : mintrans;
+    return prefix == undefined ? mintrans : mintrans ? "Rp. " + mintrans : "";
+  }
+
+  /* Fungsi formatMaxPotency */
+  var maxpotency = document.getElementById("maxpotency");
+
+  maxpotency.addEventListener("keyup", function(e){
+    maxpotency.value = formatMaxPotency(this.value, "Rp. ");
+  });
+
+  function formatMaxPotency(angka, prefix){
+    var number_string = angka.replace(/[^,\d]/g, "").toString(),
+        split         = number_string.split(","),
+        sisa          = split[0].length % 3,
+        maxpotency    = split[0].substr(0, sisa),
+        ribuan        = split[0].substr(sisa).match(/\d{3}/gi);
+    if(ribuan){
+      separator = sisa ? "." : "";
+      maxpotency += separator + ribuan.join(".");
+    }
+
+    maxpotency = split[1] != undefined ? maxpotency + "," + split[1] : maxpotency;
+    return prefix == undefined ? maxpotency : maxpotency ? "Rp. " + maxpotency : "";
+
+  }
+
+  $('#confirm-delete').on('click', function () {
+    Swal.fire({
+      title: 'Are you suddre?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Your text here!',
+      confirmButtonClass: 'btn btn-primary',
+      cancelButtonClass: 'btn btn-danger ml-1',
+      buttonsStyling: false,
+    }).then(function (result) {
+      if (result.value) {
+        Swal.fire(
+          {
+            type: "success",
+            title: 'Deleted!',
+            text: 'Your file has been deleted.',
+            confirmButtonClass: 'btn btn-success',
           }
-          else if (result.dismiss === Swal.DismissReason.cancel) {
-            Swal.fire({
-              title: 'Cancelled',
-              text: 'Your Feature is safe :)',
-              type: 'error',
-              confirmButtonClass: 'btn btn-success',
-            })
-          }
-        });
-      });
-    });
+        )
+      }
+    })
+  });
+
 </script>
-<script src="{{asset('vendors/js/extensions/dragula.min.js')}}"></script>
-<script src="{{asset('vendors/js/extensions/sweetalert2.all.min.js')}}"></script>
 <script src="{{asset('vendors/js/tables/datatable/datatables.min.js')}}"></script>
 <script src="{{asset('vendors/js/tables/datatable/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/datatables.checkboxes.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/responsive.bootstrap.min.js')}}"></script>  
+<script src="{{asset('vendors/js/forms/spinner/jquery.bootstrap-touchspin.js')}}"></script>
 @endsection
-
 {{-- page scripts --}}
 @section('page-scripts')
-<script src="{{asset('js/scripts/extensions/drag-drop.js')}}"></script>
+<script src="{{asset('js/scripts/pages/app-voucher.js')}}"></script>
+<script src="{{asset('js/scripts/forms/number-input.js')}}"></script>
 @endsection
